d158a7b63ede5cb0f1e24dcd69009f6f58481fec
Nelmeris <KufaevArtem@icloud.com>
2019-04-30 21:19:10 +0300

    ...

--- PerfectTemplate/Handlers/RemoveReviewHandler.swift
+++ PerfectTemplate/Handlers/RemoveReviewHandler.swift
@@ -2,7 +2,44 @@
 //  RemoveReviewHandler.swift
 //  PerfectTemplate
 //
-//  Created by Артем Куфаев on 30/04/2019.
+//  Created by Artem Kufaev on 30/04/2019.
 //
 
 import Foundation
+import PerfectHTTP
+
+class RemoveReviewHandler: AbstractHandler {
+    var request: HTTPRequest
+    var response: HTTPResponse
+    
+    required init(request: HTTPRequest, response: HTTPResponse) {
+        self.request = request
+        self.response = response
+    }
+}
+
+extension RemoveReviewHandler {
+    func dataValidation() -> Bool {
+        guard request.param(name: "id_comment") != nil
+            else {
+                ErrorHandler(request: request, response: response).process()
+                return false
+        }
+        return true
+    }
+    
+    func process() {
+        response.setHeader(.contentType, value: "application/json")
+        guard dataValidation() else { return }
+        let json: [String: Any] = [
+            "result": 1
+        ]
+        
+        do {
+            try response.setBody(json: json)
+        } catch {
+            print(error)
+        }
+        response.completed()
+    }
+}
